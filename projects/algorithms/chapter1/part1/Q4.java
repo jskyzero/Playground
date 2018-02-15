40b6adecf9118545b2797c14e676bf25804efb4f
jskyzero <jskyzero@outlook.com>
2018-02-15 16:19:42 +0800

    add 1.1.4

---
+++ projects/algorithms/chapter1/part1/Q4.java
@@ -0,0 +1,19 @@
+class Q4 {
+  public static void main(String[] args) {
+    // a.
+    // if (a > b) then c = 0
+    // if then end ? no, no such things in Java, we have if () {} else {}
+
+    // b.
+    // if a > b { c = 0; }
+    // if (a > b) { c = 0; }
+
+    // c
+    // if (a > b) c = 0;
+    // just ok 
+
+    // d
+    // if (a > b) c = 0; else b = 0;
+    // just od
+  }
+}
