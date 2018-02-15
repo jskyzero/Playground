f41d67bb7d5308e1d91e1d3de91809dd012ce9e8
jskyzero <jskyzero@outlook.com>
2018-02-15 13:49:19 +0800

    update chapter 1 part 1 readme and Q1

--- projects/algorithms/chapter1/part1/Q1.java
+++ projects/algorithms/chapter1/part1/Q1.java
@@ -1,5 +1,15 @@
+/*
+ * 1. double printf use '%f'
+ * 2. boolean printf use '%b'
+ */
 class Q1 {
   public static void main(String[] argv) {
-    System.out.println("Hello World!");
+    int a = (0 + 15) / 2;
+    double b = 2.0e-6 * 100000000.1;
+    boolean c = true && false || true && true;
+
+    System.out.printf("a = %d\n", a);
+    System.out.printf("b= %f\n", b);
+    System.out.printf("c = %b\n", c);
   }
 }
