6cb386241a40eae9b122a0aa84c6e74ecbe1aa38
jskyzero <jskyzero@outlook.com>
2018-02-28 11:14:06 +0800

    :pill: String != string

--- projects/algorithms/chapter1/part1/Q27.java
+++ projects/algorithms/chapter1/part1/Q27.java
@@ -4,7 +4,7 @@
 import java.util.Map;
 
 public class Q27 {
-  public static void main(string[] args) {
+  public static void main(String[] args) {
     // binomial(100, 50, 0.25)
     // 调用次数 2 ^ 100 以上
   }
@@ -17,34 +17,35 @@ public class Q27 {
     return (1.0 - p) * binomial(N - 1, k, p) + p * binomial(N - 1, k - 1, p);
   }
 
-  class Index {
-    int x, y;
-    Index(int x, int y) {
-      this.x = x;
-      this.y = y;
-    }
-  }
-
   public static double binomial(int N, int k, double p, Map<Index, Double> cache) {
     if (N == 0 && k == 0)
       return 1.0;
     if (N < 0 && k < 0)
       return 0.0;
     double left = 0, right = 0;
-    if (cache.containsKey(new Index(N - 1, k))) {
-      left = cache.get(new Index(N - 1, k)).doubleValue();
+    Index leftKey = new Index(N - 1, k);
+    if (cache.containsKey(leftKey)) {
+      left = cache.get(leftKey).doubleValue();
     } else {
-      left = binomial(index.x, index.y, p, cache);
-      cache.put(new Index(N - 1, k), left);
+      left = binomial(N - 1, k, p, cache);
+      cache.put(leftKey, left);
     }
-    if (cache.containsKey(new Index(N - 1, k - 1))) {
-      right = cache.get(new Index(N - 1, k)).doubleValue();
+    Index rightKey = new Index(N - 1, k -1);
+    if (cache.containsKey(rightKey)) {
+      right = cache.get(rightKey).doubleValue();
     } else {
-      right = binomial(index.x, index.y, p, cache);
-      cache.put(new Index(N - 1, k - 1), right);
+      right = binomial(N - 1, k - 1, p, cache);
+      cache.put(rightKey, right);
     }
 
     return (1.0 - p) * left + p * right;
   }
 }
 
+class Index {
+  int x, y;
+  Index(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+}
---
+++ projects/algorithms/chapter1/part1/Q28.java
@@ -0,0 +1,8 @@
+/**
+ * Q28
+ */
+public class Q28 {
+   public static void main(String[] args) {
+     
+   }
+}
