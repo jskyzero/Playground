5c0b1c70ec855617359d4ed5bcab0130ec19eb31
jskyzero <jskyzero@outlook.com>
2018-03-05 00:05:27 +0800

    :pill: finally finish 1.1.32 matrix

--- projects/algorithms/chapter1/part1/Q33.java
+++ projects/algorithms/chapter1/part1/Q33.java
@@ -3,7 +3,21 @@
  */
 public class Q33 {
   public static void main(String[] args) {
-    Q33Matrix matrix = new Q33Matrix<Integer>(1, 2);
-    System.out.print(matrix);
+    double[][] A = {{1, 2}, {3, 4}, {5, 6}};
+    double[][] B = {{7,8}, {9, 10}};
+
+    double[] a = {1, 2, 3};
+    double[] b = {4, 5};
+
+    System.out.printf("a * b = %5f\n", Q33Matrix.dot(a, b));
+    System.out.printf("A * B = \n");
+    Q33Matrix.print(Q33Matrix.mult(A, B));
+    System.out.printf("A * a = \n");
+    Q33Matrix.print(Q33Matrix.mult(A, a));
+    System.out.printf("a * A = \n");
+    Q33Matrix.print(Q33Matrix.mult(a, A));
+    
+    
+
   }
 }
--- projects/algorithms/chapter1/part1/Q33Matrix.java
+++ projects/algorithms/chapter1/part1/Q33Matrix.java
@@ -1,51 +1,131 @@
+import java.util.stream.IntStream;
 
 /**
  * Q33Matrix
  */
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.util.ArrayList;
+public class Q33Matrix {
 
-public class Q33Matrix<T> {
-  private int m, n;
-  private ArrayList data;
-  //  m * n
-
-  public Q33Matrix(int m, int n) {
-    this.m = m;
-    this.n = n;
-
-    data = new ArrayList<T>(size());
+  public static double dot(double[] x, double[] y) {
+    return IntStream.range(0, Math.min(x.length, y.length)).mapToDouble(i -> x[i] * y[i]).sum();
   }
 
-  public T get(int m, int n) {
-    return (T) data.get(m * colSize() + n);
-  }
+  public static double[][] mult(double[][] x, double[][] y) {
+    int m = x.length, n = y[0].length;
+    double[][] result = new double[m][n];
 
-  public int rowSize() {
-    return m;
-  }
-
-  public int colSize() {
-    return n;
-  }
-
-  public int size() {
-    return m * n;
-  }
-
-  public String toString() {
-    try {
-      ByteArrayOutputStream out = new ByteArrayOutputStream();
-      for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-          out.write(String.format("%s\n", "Hello World!").getBytes());
-        }
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        final int colIndex = j;
+        double[] col = IntStream.range(0, y.length).mapToDouble(rowIndex -> y[rowIndex][colIndex]).toArray();
+        result[i][j] = dot(x[i], col);
       }
-      return new String(out.toByteArray());
-    } catch (IOException e) {
-      return e.toString();
+    }
+
+    return result;
+  }
+
+  public static double[][] transpose(double[][] a) {
+    int m = a.length, n = a[0].length;
+    double[][] result = new double[n][m];
+
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        result[j][i] = a[n - 1 - j][m - 1 - i];
+      }
+    }
+    return result;
+  }
+
+  public static double[] mult(double[][] a, double[] x) {
+    int m = a.length, n = Math.min(a[0].length, x.length);
+    double[] result = new double[m];
+    for (int i = 0; i < m; i++) {
+      result[i] = dot(a[i], x);
+    }
+    return result;
+  }
+
+  public static double[] mult(double[] x, double[][] a) {
+    int m = Math.min(a.length, x.length), n = a[0].length;
+    double[] result = new double[n];
+    for (int i = 0; i < n; i++) {
+      final int colIndex = i;
+      double[] col = IntStream.range(0, m).mapToDouble(rowIndex -> a[rowIndex][colIndex]).toArray();
+      result[i] = dot(x, col);
+    }
+    return result;
+  }
+
+  public static void print(double[][] matrix) {
+    int m = matrix.length, n = matrix[0].length;
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        System.out.printf("%5f ", matrix[i][j]);
+      }
+      System.out.println();
     }
   }
+
+  public static void print(double[] array) {
+    IntStream.range(0, array.length).forEach(i -> System.out.printf("%5f%s", array[i], i + 1 == array.length ? "\n" : " "));
+  }
 }
+
+// bellow is a test example, but in fact don't need to be such complex
+// make things simple please
+
+// 
+// import java.io.ByteArrayOutputStream;
+// import java.io.IOException;
+// import java.util.ArrayList;
+
+// public class Q33Matrix<T> {
+//   private int m, n;
+//   private ArrayList data;
+//   //  m * n
+
+//   public Q33Matrix(int m, int n) {
+//     this.m = m;
+//     this.n = n;
+
+//     data = new ArrayList<T>(size());
+//     System.out.print(size());
+//     System.out.print(data.size());
+//   }
+
+//   public T get(int m, int n) {
+//     return (T) data.get(index(m, n));
+//   }
+
+//   public int rowSize() {
+//     return m;
+//   }
+
+//   public int colSize() {
+//     return n;
+//   }
+
+//   public int size() {
+//     return m * n;
+//   }
+
+//   private int index(int raw, int col) {
+//     return raw * colSize() + col;
+//   }
+
+//   public String toString() {
+//     try {
+//       ByteArrayOutputStream out = new ByteArrayOutputStream();
+//       for (int i = 0; i < m; i++) {
+//         for (int j = 0; j < n; j++) {
+//           out.write(String.format("%s\n", get(i, j).toString()).getBytes());
+//         }
+//         out.write("\n".getBytes());
+//       }
+//       return new String(out.toByteArray());
+//     } catch (IOException e) {
+//       return e.toString();
+//     }
+//   }
+// }
