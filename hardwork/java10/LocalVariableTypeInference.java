8e43e8888d45e4c3759ff23f7c2b87a1f8672896
jskyzero <jskyzero@outlook.com>
2018-03-21 14:57:04 +0800

    add java10 local variable type inference

---
+++ hardwork/java10/LocalVariableTypeInference.java
@@ -0,0 +1,9 @@
+/**
+ * LocalVariableTypeInference
+ */
+public class LocalVariableTypeInference {
+  public static void main(String[] args) {
+    var str = "Hello World!";
+    System.out.print(str);
+  }
+}
